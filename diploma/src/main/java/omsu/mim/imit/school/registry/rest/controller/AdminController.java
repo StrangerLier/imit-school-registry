2b4565db31f64eda4d6253e87c46ac559e11a5f8
KozlovDi <kozlovdmitriy123890@gmail.com>
2025-02-20 20:34:48 +0600

    Fix CrossOrigin

--- diploma/src/main/java/omsu/mim/imit/school/registry/rest/controller/AdminController.java
+++ diploma/src/main/java/omsu/mim/imit/school/registry/rest/controller/AdminController.java
@@ -21,25 +21,26 @@ public class AdminController {
     private final ChildService childService;
 
     @GetMapping("/admin/v1/filter")
-    @CrossOrigin
+    @CrossOrigin(origins = {"https://dip.rkkm.space", "https://dipapi.rkkm.space"})
     public ResponseEntity<List<ChildRestResponse>> filter(FilterChildrenRequestDto request) {
         request.correctParams();
         return ResponseEntity.ok(childService.filter(request));
     }
 
     @DeleteMapping("/admin/v1/delete")
+    @CrossOrigin(origins = {"https://dip.rkkm.space", "https://dipapi.rkkm.space"})
     public ResponseEntity<ChildRestResponse> delete(@RequestParam UUID id) {
         return ResponseEntity.ok(childService.deleteById(id));
     }
 
     @PostMapping("/admin/v1/deactivateChildren")
-    @CrossOrigin
+    @CrossOrigin(origins = {"https://dip.rkkm.space", "https://dipapi.rkkm.space"})
     public ResponseEntity<ChildRestResponse> activateChildren(@RequestParam UUID id) {
         return ResponseEntity.ok(childService.changeStatus(id, ChildStatus.APPROVED));
     }
 
     @PostMapping("/admin/v1/changeStatus")
-    @CrossOrigin
+    @CrossOrigin(origins = {"https://dip.rkkm.space", "https://dipapi.rkkm.space"})
     public ResponseEntity<ChildRestResponse> deactivateChildren(@RequestParam UUID id, @RequestParam ChildStatus status) {
         return ResponseEntity.ok(childService.changeStatus(id, status));
     }
