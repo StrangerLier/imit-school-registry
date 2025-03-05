f6f909f196ef6b36bd71a343fa0afdc28d9dbb1f
Vad1m <vadim.grigorev@gmail.com>
2025-03-05 15:22:59 +0300

    Disable cors

--- diploma/src/main/java/omsu/mim/imit/school/registry/config/SecurityConfig.java
+++ diploma/src/main/java/omsu/mim/imit/school/registry/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(
                     auth -> {
                         auth.requestMatchers("/admin/**").hasRole(SecurityConstants.ADMIN_ROLE)
