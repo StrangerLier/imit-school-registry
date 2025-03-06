1d43e0c440a030655f376602ebadab0ad893da72
Vad1m <vadim.grigorev@gmail.com>
2025-03-06 22:26:10 +0300

    Cors fix

--- diploma/src/main/java/omsu/mim/imit/school/registry/config/WebConfig.java
+++ diploma/src/main/java/omsu/mim/imit/school/registry/config/WebConfig.java
@@ -9,6 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(false);
     }
 }
