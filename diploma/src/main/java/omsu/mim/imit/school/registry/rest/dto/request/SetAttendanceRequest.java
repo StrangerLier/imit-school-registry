c05d3ce5ac2c9ba967e78cc62e99c961e11a3eae
KozlovDi <kozlovdmitriy123890@gmail.com>
2025-06-14 20:34:56 +0600

    Add work with journals

---
+++ diploma/src/main/java/omsu/mim/imit/school/registry/rest/dto/request/SetAttendanceRequest.java
@@ -0,0 +1,22 @@
+package omsu.mim.imit.school.registry.rest.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SetAttendanceRequest {
+
+    private UUID id;
+
+    private Boolean isAttend;
+
+    private String comment;
+
+}
